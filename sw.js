a14fed8d5eb20a9fee9fcde2eee76c91a2f66fcd
linaticsan <acharyasanskarsan272@gmail.com>
2026-09-25 00:01:58 +0900

    Step 10 bump safe app shell to v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX='pocket-ai-web-shell-';
-const CACHE=APP_CACHE_PREFIX+'v8';
+const CACHE=APP_CACHE_PREFIX+'v9';
 const LEGACY_APP_CACHES=['pocket-ai-web-step1-css'];
 
 const ASSETS=["./","./index.html","./api-hub.js","./app.js","./boot-v83.css","./boot-v83.js","./chat-v77.css","./coding-v1.css","./coding-v1.js","./desktop-workspace-v115.js","./feature-v2.js","./files-v31.css","./files-v32.js","./files-v80.css","./icon.svg","./icons-v132.css","./icons-v132.js","./library-online-v34.css","./library-online-v34.js","./library-v33.css","./library-v33.js","./local-oneclick.js","./manifest.webmanifest","./mobile-v30.css","./motion-v32.css","./photo-ui-v100.js","./polish-v84.css","./polish-v84.js","./qa-v79.js","./reference-ui-v89.js","./responsive-v92.js","./simple-v87.css","./ui-core.css","./ui-v59.css","./ui-v59.js","./ux-v39.js","./v3-guard.js","./v3-hotfix.css","./v3.css","./v3.js","./visibility-v82.css","./webnovel-v42.css","./webnovel-v42.js","./workspace.js"];
