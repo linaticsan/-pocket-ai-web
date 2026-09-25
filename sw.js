6cf0777706da5a320e2ea22fb625aef11b4602fc
linaticsan <acharyasanskarsan272@gmail.com>
2026-09-25 10:51:23 +0900

    Step 19 bump safe app shell to v19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX='pocket-ai-web-shell-';
-const CACHE=APP_CACHE_PREFIX+'v18';
+const CACHE=APP_CACHE_PREFIX+'v19';
 const LEGACY_APP_CACHES=['pocket-ai-web-step1-css'];
 
 const CORE_ASSETS=[
