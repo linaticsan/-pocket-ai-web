680646eb5b33523389f21f8060dab38b63404d93
linaticsan <acharyasanskarsan272@gmail.com>
2026-09-26 15:21:12 +0900

    chore: bump coherent app shell to STEP 30

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const APP_CACHE_PREFIX='pocket-ai-web-shell-';
-const CACHE=APP_CACHE_PREFIX+'v34';
-const BUILD='step29-cache-coherence';
+const CACHE=APP_CACHE_PREFIX+'v35';
+const BUILD='step30-canonical-nav-cleanup';
 const LEGACY_APP_CACHES=['pocket-ai-web-step1-css'];
 
 const CORE_ASSETS=[
